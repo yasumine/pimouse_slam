f4fca8574b9043503cce7ff2ef9d77a507900090
yasumine <s206265@center.wakayama-u.ac.jp>
2019-05-17 11:02:02 +0000

    mapムズ

--- scripts/keyboard_cmd_vel.py
+++ scripts/keyboard_cmd_vel.py
@@ -6,7 +6,7 @@ from std_srvs.srv import Trigger, TriggerResponse
 rospy.wait_for_service('/motor_on')
 rospy.wait_for_service('/motor_off')
 rospy.on_shutdown(rospy.ServiceProxy('/motor_off',Trigger).call)
-rospy.ServiceProxy('/motor_on',Trigger).call
+rospy.ServiceProxy('/motor_on',Trigger).call()
 
 rospy.init_node('keyboard_cmd_vel')
 pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
@@ -15,8 +15,8 @@ while not rospy.is_shutdown():
     direction = raw_input('k: forward, j: backward, h: left, l: right, return: stop >')
     if 'k' in direction: vel.linear.x = 0.15
     if 'j' in direction: vel.linear.x = -0.15
-    if 'h' in direction: vel.linear.z = 3.14/4
-    if 'k' in direction: vel.linear.z = -3.14/4
+    if 'h' in direction: vel.angular.z = 3.14/4
+    if 'l' in direction: vel.angular.z = -3.14/4
     print vel
     pub.publish(vel)
     
